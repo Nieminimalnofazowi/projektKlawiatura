e5d9b7df0665a1df3d16807e1738888157ccd8dd
pnikrat <sniffoza@gmail.com>
2015-04-04 22:23:42 +0200

    user class
    troche kodu do klasy user

--- user.cpp
+++ user.cpp
@@ -28,7 +28,7 @@ bool user::saveStats(QVector<int> currentSessionStats)
 
 bool user::saveStatsToFile()
 {
-    QFile plikWynikowy("C:\\Results.txt");
+    QFile plikWynikowy("Results.txt");
     if(!plikWynikowy.open(QIODevice::ReadWrite | QIODevice::Text))
         return 0;
     QTextStream out(&plikWynikowy);
@@ -37,8 +37,22 @@ bool user::saveStatsToFile()
     QString temp = in.readAll();
     if(temp.contains(this->userID))
     {
-
+        int tempElement = temp.indexOf(this->userID,0); //szukamy userID od pierwszego znaku
+        tempElement += this->userID.length(); //wyliczam za ktora litera stringu mam wstawic wyniki
+        //stworzenie stringu wynikow
+        QString tempStats;
+        for(int i = 0 ; i<stats.size() ; i++)
+        {
+            for(int j = 0 ; j<stats[i].length() ; j++)
+            {
+                tempStats = tempStats + " " + QString::number(stats[i][j]); //jeden ciag danych
+            }
+            tempStats += "\n"; //new line
+        }
+        //za chwile
+        temp.insert(tempElement,tempStats);
     }
+    out << temp; //wgrywamy z powrotem do pliku to co bylo i dopisano
     plikWynikowy.close();
     return 1;
 }
